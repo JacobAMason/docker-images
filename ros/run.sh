487ae58e164f3f9ec85f449bdb726121ae27d099
Jacob Mason <jacob@jacobmason.net>
2016-11-18 23:08:43 -0600

    Adds a bunch of dependencies for my ros docker image

--- ros/run.sh
+++ ros/run.sh
@@ -1,7 +1,15 @@
-docker run -it \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -e DISPLAY=$DISPLAY \
-    -e XAUTHORITY=$XAUTHORITY \
-    -v $XAUTHORITY:$XAUTHORITY \
-    -e QT_X11_NO_MITSHM=1 \
+xhost +local:root
+nvidia-docker run -it \
+    --user=$(id -u) \
+    --env="DISPLAY" \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    -env="QT_X11_NO_MITSHM=1" \
+    -v /home/jacob:/home/jacob \
+    -w="/home/jacob/stateSpaceRobotics/robot2017" \
+    -p 22 \
     ros:indigo-fixes
+xhost -local:root
